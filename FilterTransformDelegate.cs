c067f5190b3a3b8cd2b3a88f3855b7abb6327453
Abdulkadir Ali Mohammed <dadorap88@hotmail.com>
2024-11-05 15:46:53 +0100

    Add project files.

---
+++ FilterTransformDelegate.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsDelegateTwo
+{
+    public delegate TOutput FilterTransformDelegate<TInput,TOutput>(TInput input);
+        
+   
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿namespace GenericsDelegateTwo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string>()
+            {
+                "Dad",
+                "Ali",
+                "Moh"
+            };
+
+            FilterTransformDelegate<string, string> transofrm = x => x.Length > 3 ? x : default;
+            var filtList = FilterAndTransform<string, string>(list, transofrm);
+
+            foreach (var filt in filtList)
+            {
+                Console.WriteLine(filt);
+            }
+        }
+           public static List<TOutput> FilterAndTransform<TInput, TOutput>(List<TInput> items, FilterTransformDelegate<TInput, TOutput> filterTransform)
+        {
+            List<TOutput> outputs = new List<TOutput>();
+
+            foreach (var item in items)
+            {
+               
+            var transformed = filterTransform(item);
+                if (!Equals(transformed, default(TOutput)))
+                {
+                    outputs.Add(transformed);
+                }
+
+            }
+
+            return outputs;
+        }
+        
+    }
+}
